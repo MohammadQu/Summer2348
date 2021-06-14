efebeff8ce1cda11a619fb4ed06a0355c85b760b
MohammadQu <60757189+MohammadQu@users.noreply.github.com>
2021-06-14 00:42:39 -0500

    Add files via upload

---
+++ Homework1/ZyLab1.20.py
@@ -0,0 +1,18 @@
+# Mohammad Qureshi
+# PSID 1789301
+# LAB 1.20
+
+
+
+#input first integer
+print('Enter integer:')
+user_num = int(input())
+print('You entered:', user_num)
+print(user_num, 'squared is', user_num * user_num)
+print('And', user_num, 'cubed is', user_num * user_num * user_num, '!!')
+
+#input second integer
+print('Enter another integer:')
+user_num1 = int(input())
+print(user_num, '+', user_num1, 'is', user_num +user_num1)
+print(user_num, '*', user_num1, 'is', user_num *user_num1)
